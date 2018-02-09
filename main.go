7f742aef7d88985455095189f811b8778ef3e820
Sara Frandsen <sara.frandsen@gmail.com>
2018-02-09 15:31:40 -0800

    finish part 1 of quiz exercise

--- main.go
+++ main.go
@@ -1,9 +1,58 @@
 package main
 
-import "flag" // specify options for command-line programs
+import (
+	"encoding/csv"
+	"flag" // specify options for command-line programs
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of a 'question,answer'")
 	flag.Parse()
-	_ = csvFilename
+	file, err := os.Open(*csvFilename) // pointer to a string (how the flag package works)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1) // status code 1: something went wrong
 }
